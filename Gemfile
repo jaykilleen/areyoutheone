aeabadd2e6ba85aaff994a9c41240dc5bdba83fc
jaykilleen <me@jaykilleen.com>
2017-05-21 21:08:41 +1000

    Add user model with basic email validation. Setup basic framework for seeds, fixtures, factories and shoulda matches

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.5'
+  gem "factory_girl_rails", "~> 4.0"
+  gem 'shoulda-matchers', git: 'https://github.com/thoughtbot/shoulda-matchers.git', branch: 'rails-5'
+  gem 'database_cleaner'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation, {:only => %w[users]}
+DatabaseCleaner.clean
+
+puts "--------------------PROCESSING USERS.CSV--------------------"
+users_csv = File.read(Rails.root.join('lib', 'seeds', 'users.csv'))
+users_data = CSV.parse(users_csv, :headers => true)
+users_data.each do |row|
+  user = User.new
+  user.email = row['email']
+  user.save
+  if user.save
+    puts "User => #{user.email} saved"
+  else
+    puts "User => #{user.email} failed to save XXXXXXXXXX"
+    puts user.errors.full_messages
+  end
+end
+puts "--------------------FINISHED USERS.CSV--------------------"
+
+
+puts "--------------------FINISHED PROCESSING CSVs--------------------"
+puts "There are now #{User.count} rows in the users table"
